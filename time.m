f4c4d635f257ab8afca68ee4a19bd3b14a15d3d7
jgte <j.g.deteixeiradaencarnacao@tudelft.nl>
2016-12-02 13:06:44 -0600

    moved time-related functions previously hidden in simpletimeseries here

--- time.m
+++ time.m
@@ -159,5 +159,158 @@ classdef time
         tic
       end
     end
+    function [date, doy, dow] = gps2date(gps_week, gps_sow)
+      % SYNTAX:
+      %   [date, doy, dow] = gps2date(gps_week, gps_sow);
+      %
+      % INPUT:
+      %   gps_week = GPS week
+      %   gps_sow  = GPS seconds of week
+      %
+      % OUTPUT:
+      %   date = date [year month day hour min sec]
+      %   doy  = day of year
+      %   dow  = day of week
+      %
+      % DESCRIPTION:
+      %   Conversion from GPS time to calendar date and day of year (DOY).
+
+      %----------------------------------------------------------------------------------------------
+      %                           goGPS v0.4.3
+      %
+      % Copyright (C) 2009-2014 Mirko Reguzzoni, Eugenio Realini
+      %----------------------------------------------------------------------------------------------
+      %
+      %    This program is free software: you can redistribute it and/or modify
+      %    it under the terms of the GNU General Public License as published by
+      %    the Free Software Foundation, either version 3 of the License, or
+      %    (at your option) any later version.
+      %
+      %    This program is distributed in the hope that it will be useful,
+      %    but WITHOUT ANY WARRANTY; without even the implied warranty of
+      %    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      %    GNU General Public License for more details.
+      %
+      %    You should have received a copy of the GNU General Public License
+      %    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+      %----------------------------------------------------------------------------------------------
+
+      gps_start_datenum = 723186; %This is datenum([1980,1,6,0,0,0])
+
+      gps_dow = fix(gps_sow/86400);                             %day of week
+      date = datevec(gps_start_datenum + 7*gps_week + gps_dow); %calendar date up to days
+      gps_sod = gps_sow - gps_dow*86400;                        %seconds of day
+      date(:,4) = floor(gps_sod/3600);                          %hours
+      date(:,5) = floor(gps_sod/60 - date(:,4)*60);             %minutes
+      date(:,6) = gps_sod - date(:,4)*3600 - date(:,5)*60;      %seconds
+
+      %day of year (DOY)
+      if (nargout > 1)
+          doy = date2doy(datenum(date));
+          doy = floor(doy);
+      end
+
+      %day of week
+      if (nargout > 2)
+          dow = gps_dow;
+      end
+    end
+    function [gps_week, gps_sow, gps_dow] = date2gps(date)
+      % SYNTAX:
+      %   [gps_week, gps_sow, gps_dow] = date2gps(date);
+      %
+      % INPUT:
+      %   date = date [year, month, day, hour, min, sec]
+      %
+      % OUTPUT:
+      %   gps_week = GPS week
+      %   gps_sow  = GPS seconds of week
+      %   gps_dow  = GPS day of week
+      %
+      % DESCRIPTION:
+      %   Conversion from calendar date to GPS time.
+
+      %----------------------------------------------------------------------------------------------
+      %                           goGPS v0.4.3
+      %
+      % Copyright (C) 2009-2014 Mirko Reguzzoni, Eugenio Realini
+      %----------------------------------------------------------------------------------------------
+      %
+      %    This program is free software: you can redistribute it and/or modify
+      %    it under the terms of the GNU General Public License as published by
+      %    the Free Software Foundation, either version 3 of the License, or
+      %    (at your option) any later version.
+      %
+      %    This program is distributed in the hope that it will be useful,
+      %    but WITHOUT ANY WARRANTY; without even the implied warranty of
+      %    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      %    GNU General Public License for more details.
+      %
+      %    You should have received a copy of the GNU General Public License
+      %    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+      %----------------------------------------------------------------------------------------------
+
+      gps_start_datenum = 723186; %This is datenum([1980,1,6,0,0,0])
+
+      %number of days since the beginning of GPS time
+      deltat   = (datenum([date(:,1), date(:,2), date(:,3)]) - gps_start_datenum);
+
+      gps_week = floor(deltat/7);            %GPS week
+      gps_dow  = floor(deltat - gps_week*7); %GPS day of week
+      gps_sow  = (deltat - gps_week*7)*86400; 
+      gps_sow = gps_sow + date(:,4)*3600 + date(:,5)*60 + date(:,6); %GPS seconds of week
+    end
+    function [doy,fraction] = date2doy(inputDate)
+    %DATE2DOY  Converts the date to decimal day of the year.
+      %   [doy,fraction] = date2doy(inputDate)
+      %
+      %   Descriptions of Input Variables:
+      %   inputDate:  Input date as a MATLAB serial datenumber
+      %
+      %   Descriptions of Output Variables:
+      %   doy: Decimal day of the year. For instance, an output of 1.5 would 
+      %       indicate that the time is noon on January 1.
+      %   fraction: Outputs the fraction of a year that has elapsed by the input
+      %       date.
+      %
+      %   Example(s):
+      %   >> [doy,frac] = date2doy(datenum('1/25/2004'));
+      %
+      %   See also:
+
+      % Author: Anthony Kendall
+      % Contact: anthony [dot] kendall [at] gmail [dot] com
+      % Created: 2008-03-11
+      % Copyright 2008 Michigan State University.
+
+      %Want inputs in rowwise format
+      [doy,fraction] = deal(zeros(size(inputDate)));
+      inputDate = inputDate(:);
+
+      %Parse the inputDate
+      [dateVector] = datevec(inputDate);
+
+      %Set everything in the date vector to 0 except for the year
+      dateVector(:,2:end) = 0;
+      dateYearBegin = datenum(dateVector);
+
+      %Calculate the day of the year
+      doyRow = inputDate - dateYearBegin;
+
+      %Optionally, calculate the fraction of the year that has elapsed
+      flagFrac = (nargout==2);
+      if flagFrac
+          %Set the date as the end of the year
+          dateVector(:,1) = dateVector(:,1) + 1;
+          dateYearEnd = datenum(dateVector);
+          fracRow = (doyRow - 1) ./ (dateYearEnd - dateYearBegin);
+      end
+
+      %Fill appropriately-sized output array
+      doy(:) = doyRow;
+      if flagFrac
+          fraction(:) = fracRow;
+      end
+    end
   end
 end
