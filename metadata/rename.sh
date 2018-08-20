60a6b7e399c95344377231b975dff1b1418ff3d7
jgte <j.g.deteixeiradaencarnacao@tudelft.nl>
2018-08-20 13:26:38 +0100

    finalized the metadata utlity scripts

--- metadata/rename.sh
+++ metadata/rename.sh
@@ -2,14 +2,11 @@
 
 # renames data and metadata
 
-echo "#NOTICE: This is untested!!!"
-
 METADATADIR=$(cd $(dirname BASH_SOURCE);pwd)
 DATADIR=$(cd $METADATADIR/../data/;pwd)
 RETIREDIR='retired'
 OBSOLETEDIR='obsolete'
 
-FIND_ARGS_SKIP_DIRS="-not -wholename \*$RETIREDIR\* -and -not -wholename \*$OBSOLETEDIR\*"
 ECHO=
 
 case $# in
@@ -31,49 +28,56 @@ case $# in
   ;;
 esac
 
-METADATA_OLD=$(find $METADATADIR $FIND_ARGS_SKIP_DIRS -name \*$1\*)
+METADATA_OLD=$(find $METADATADIR -name \*$1\* | grep -v "/$RETIREDIR/" | grep -v "/$OBSOLETEDIR/")
 METADATA_NEW=$METADATADIR/$(basename ${METADATA_OLD/$1/$2})
 
 if [ $(echo "$METADATA_OLD" | wc -l) -eq 0 ]
 then  
-  echo "ERROR: cannot find any metadata called *$i*"
+  echo "ERROR: cannot find any metadata called *$METADATA_OLD*"
   exit 3
 elif [ $(echo "$METADATA_OLD" | wc -l) -gt 1 ]; then
-  echo "ERROR: there are several metadata called *$i*:"
+  echo "ERROR: there are several metadata called *$METADATA_OLD*:"
   echo "$METADATA_OLD"
   echo "Can only handle one metadata at a time"
   exit 3
 fi
 
-if [ -z "$ECHO" ]
-then
-  read -p "Renaming $(basename $METADATA_OLD) to $(basename $METADATA_NEW)? [Y/n]" ANSWER
-  if [ "$ANSWER" == "N" ] || [ "$ANSWER" == "n" ]
-  then
-    echo "Nothing done..."
-    exit
-  fi
-fi
+# if [ -z "$ECHO" ]
+# then
+#   read -p "Renaming $(basename $METADATA_OLD) to $(basename $METADATA_NEW)? [Y/n]" ANSWER
+#   if [ "$ANSWER" == "N" ] || [ "$ANSWER" == "n" ]
+#   then
+#     echo "Nothing done..."
+#     exit
+#   fi
+# fi
 
-#rename metadata
-$ECHO mv -v $METADATA_OLD $METADATA_NEW
 
 #define the old and new metadata roots
 MTDR_OLD=$(basename ${METADATA_OLD/.metadata})
 MTDR_NEW=$(basename ${METADATA_NEW/.metadata})
 
-#loop over source data dir(s) and rename them
-for i in $(find $DATADIR/ -type d -name $MTDR_OLD\*)
+if [ -e $DATADIR/$MTDR_NEW ]
+then
+  echo "ERROR: directory $DATADIR/$MTDR_NEW exists, cannot renamed"
+  exit 3
+fi
+
+#make new data dir
+$ECHO mkdir -p $DATADIR/$MTDR_NEW
+
+#rename all files in this new dir
+for j in $(find $DATADIR/$MTDR_OLD -name $MTDR_OLD\* -not -type d)
 do
-  #make new data dir
-  $ECHO mkdir $DATADIR/$MTDR_NEW
-  #rename all files in this dir and move them to the new dir
-  for j in $(find $i -name $MTDR_OLD\*)
-  do
-    #rename data 
-    $ECHO mv -v $j $DATADIR/$MTDR_NEW/$(basename ${j/$MTDR_OLD/$MTDR_NEW})
-  done
+  #rename data 
+  $ECHO mv -v $j $DATADIR/$MTDR_NEW/$(basename ${j//$MTDR_OLD/$MTDR_NEW})
 done
+
+#rename metadata
+$ECHO mv -v $METADATA_OLD $METADATA_NEW
+
+#remove old dir
+rmdir $DATADIR/$MTDR_OLD 
 
 #check for references to this metadata in remaining metadata files
 OUT=$(grep -l $(basename ${MTDR_OLD/.metadata}) *.metadata)
