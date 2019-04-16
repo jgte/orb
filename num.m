d17414048005c3833b1b49a822830f7ad2fde42f
jgte <j.g.deteixeiradaencarnacao@tudelft.nl>
2019-04-16 17:55:17 -0500

    rearranged some methods

--- num.m
+++ num.m
@@ -1,62 +1,99 @@
 classdef num
   methods(Static)
-     function out=cov(x,y)
-      n=size(x,2);
-      if n==0
-        out=0;
-        return
-      end
-      if any(size(x)~=size(y))
-        out=NaN;
-        return
-      end
-      out=size(1,n);
-      for i=1:n
-        out(i)=sum(...
-          (x(:,i)-mean(x(:,i))).*...
-          (y(:,i)-mean(y(:,i)))...
-        )/size(x,1);
+    %% utils
+    % converts cell array to numeric array if all entries are numeric
+    function inout=cell(inout)
+      if iscell(inout) && all(cellfun(@isnumeric,inout))
+        inout=cell2mat(inout);
       end
     end
-    function out=corrcoef(x,y)
-      n=min([size(x,2),size(y,2)]);
-      if n==0
-        out=0;
+    function out=odd(in)
+      if ~isscalar(in)
+        out=arrayfun(@num.odd,in);
         return
       end
-      out=size(1,n);
-      for i=1:n
-        if any([std(x(:,i)),std(y(:,i))]==0)
-          out(i)=0;
-        else
-          out(i)=num.cov(x(:,i),y(:,i))./std(x(:,i),1)./std(y(:,i),1);
+      if mod(in,2)==0
+        out=in-sign(in);
+      else
+        out=in;
+      end
+    end
+    function out=even(in)
+      if ~isscalar(in)
+        out=arrayfun(@num.even,in);
+        return
+      end
+      if mod(in,2)==0
+        out=in;
+      else
+        out=in-sign(in);
+      end
+    end
+    % shortcut for [s.field(:,column)]
+    function out=struct_deal(s,field,line,column)
+      if ~xor(isempty(line),isempty(column))
+        error([mfilename,': (only) one of ''line'' or ''column'' must be empty.'])
+      end
+      if isempty(line)
+        out=zeros(size(s(1).(field),1),numel(s));
+        for i=1:numel(s)
+          out(:,i)=s(i).(field)(:,column);
+        end
+      else
+        out=zeros(numel(s),size(s(1).(field),2));
+        for i=1:numel(s)
+          out(i,:)=s(i).(field)(line,:);
         end
       end
     end
-    function plot_pardecomp(a)
+    %removes NaNs from the extremeties of 'in'
+    function out=trim_NaN(in)
+      transpose_flag=false;
+      %need column vector
+      if isvector(in) && size(in,2)>1; transpose_flag=true; end
+      %enforce transpose
+      if transpose_flag; in=transpose(in); end
+      %init loop
+      out=true(size(in));
+      in_isnan=isnan(in);
+      for j=1:size(in,2)
+        for i=1:size(in,1)
+          if ~in_isnan(i,j); break; end
+          out(i,j)=false;
+        end
+      end
+      for j=size(in,2):-1:1
+        for i=size(in,1):-1:1
+          if ~in_isnan(i,j) || ~out(i,j); break; end
+          out(i,j)=false;
+        end
+      end
+    end
+    %% pardecomp stuff (deprecated)
+    function plot_pardecomp(pd_struct)
       s=200+[0,0,21,9]*50;
       figure('Position',s,'PaperPosition',s)
-      legend_str=cell(1,numel(a.polynomial)+numel(a.sinusoidal)+numel(a.cosinusoidal)+2);
-      plot(a.in.t,a.in.y,'b','LineWidth',2), hold on
+      legend_str=cell(1,numel(pd_struct.polynomial)+numel(pd_struct.sinusoidal)+numel(pd_struct.cosinusoidal)+2);
+      plot(pd_struct.in.t,pd_struct.in.y,'b','LineWidth',2), hold on
       c=0;
       c=c+1;legend_str{c}='residual';
-      plot(a.in.t,a.y_res,'k','LineWidth',2)
+      plot(pd_struct.in.t,pd_struct.y_res,'k','LineWidth',2)
       c=c+1;legend_str{c}='original';
-      for i=1:numel(a.polynomial)
-        plot(a.in.t,a.y_polynomial(:,i),'r','LineWidth',2)
-        c=c+1;legend_str{c}=['t^',num2str(i-1),':',num2str(a.polynomial(i))];
+      for i=1:numel(pd_struct.polynomial)
+        plot(pd_struct.in.t,pd_struct.y_polynomial(:,i),'r','LineWidth',2)
+        c=c+1;legend_str{c}=['t^',num2str(i-1),':',num2str(pd_struct.polynomial(i))];
       end
-      for i=1:numel(a.sinusoidal)
-        plot(a.in.t,a.y_sinusoidal(:,i),'g','LineWidth',2)
-        c=c+1;legend_str{c}=['sin_',num2str(i),':',num2str(a.sinusoidal(i))];
+      for i=1:numel(pd_struct.sinusoidal)
+        plot(pd_struct.in.t,pd_struct.y_sinusoidal(:,i),'g','LineWidth',2)
+        c=c+1;legend_str{c}=['sin_',num2str(i),':',num2str(pd_struct.sinusoidal(i))];
       end
-      for i=1:numel(a.cosinusoidal)
-        plot(a.in.t,a.y_cosinusoidal(:,i),'m','LineWidth',2)
-        c=c+1;legend_str{c}=['cos_',num2str(i),':',num2str(a.cosinusoidal(i))];
+      for i=1:numel(pd_struct.cosinusoidal)
+        plot(pd_struct.in.t,pd_struct.y_cosinusoidal(:,i),'m','LineWidth',2)
+        c=c+1;legend_str{c}=['cos_',num2str(i),':',num2str(pd_struct.cosinusoidal(i))];
       end
       legend(legend_str,'location','eastoutside')
-      title(['norm(x+res-y)=',num2str(norm(sum([a.y_polynomial,a.y_sinusoidal,a.y_cosinusoidal,a.y_res,-a.in.y],2))),...
-        newline,'T=',num2str(a.in.T(:)')])
+      title(['norm(x+res-y)=',num2str(norm(sum([pd_struct.y_polynomial,pd_struct.y_sinusoidal,pd_struct.y_cosinusoidal,pd_struct.y_res,-pd_struct.in.y],2))),...
+        newline,'T=',num2str(pd_struct.in.T(:)')])
       fs=16;
       set(    gca,          'FontSize',fs);
       set(get(gca,'Title' ),'FontSize',round(fs*1.3));
@@ -64,8 +101,8 @@ classdef num
       set(get(gca,'YLabel'),'FontSize',round(fs*1.2));
       grid on
     end
-    function out=save_pardecomp(t,T,x,y,y_mod,y_res,np,ns)
-      out=struct(...
+    function pd_struct=save_pardecomp(t,T,x,y,y_mod,y_res,np,ns)
+      pd_struct=struct(...
         'in',struct(... %this is just record keeping
           't',t,...
           'y',y,...
@@ -88,6 +125,36 @@ classdef num
         num.pardecomp([0;1],[-1;-1],'mode','test');
         return
       end
+% add input arguments and plot metadata to collection of parameters 'v'
+      v=varargs.wrap('sources',{....
+        {...
+          't',         [], @(i) (isnumeric(i) && isvector(i) && all(size(i)==size(t)) && ~any(isnan(i))) || isempty(i) ;...
+          'y',         [], @(i) (isnumeric(i) && isvector(i) && all(size(i)==size(t)) && ~any(isnan(i))) || isempty(i) ;...
+          'polynomial', 2,                               @(i) (isnumeric(i) && isscalar(i)) || isempty(i);...
+          'sinusoidal',[2*min(diff(t)),(t(end)-t(1))/2], @(i) isnumeric(i) || isempty(i);...
+          't0',        [],                               @(i) isnumeric(i) && isscalar(i);...
+          'mode',      'pd_struct',                      @(i) ischar(i);...
+          'x',         [],                               @(i) isnumeric(i) || isempty(i);...
+          'pd_struct', [],                               @(i) isstruct(i)  || isempty(i);...
+        },...
+      },varargin{:});
+      %patch derived parameters
+      if ~isempty(v.t) && isempty(v.t0)
+        v.t0=t(1);
+      end
+      if ~isempty(v.pd_struct)
+        if isempty(v.t); v.t=v.pd_struct.in.t; end
+        if isempty(v.y); v.y=v.pd_struct.in.y; end
+        if isempty(v.T); v.sinusoidal=v.pd_struct.in.T; end
+        
+%         continuar aqui
+        
+      end
+      %some sanity
+      if isempty(y)
+        assert(strcmp(p.Results.mode,'model'),'if input ''y'' is empty, then mode must be ''model''')
+        y=ones(size(t));
+      end
       p=inputParser;
       p.KeepUnmatched=true;
       p.addRequired( 't', @(i)  isnumeric(i) && isvector(i) && size(i,2)==1          && ~any(isnan(i)) );
@@ -97,16 +164,13 @@ classdef num
       %sinusoidal periods (in implicit units):
       p.addParameter('sinusoidal',[2*min(diff(t)),(t(end)-t(1))/2], @(i) isnumeric(i) || isempty(i));
       p.addParameter('t0',        t(1),                             @(i) isnumeric(i) && isscalar(i));
-      p.addParameter('mode',      'struct',                         @(i) ischar(i));
+      p.addParameter('mode',      'pd_struct',                      @(i) ischar(i));
       %these parameters are only valid for the "model" mode.
       p.addParameter('x',         [],                               @(i) isnumeric(i) || isempty(i));
+      p.addParameter('pd_struct', [],                               @(i) isstruct(i)  || isempty(i));
       % parse it
       p.parse(t,y,varargin{:});
-      %some sanity
-      if isempty(y)
-        assert(strcmp(p.Results.mode,'model'),'if input ''y'' is empty, then mode must be ''model''')
-        y=ones(size(t));
-      end
+
       %simpler names
       np=p.Results.polynomial;
       ns=numel(p.Results.sinusoidal);
@@ -156,14 +220,18 @@ classdef num
           %add noise
           y=y+randn_scale*randn(size(y));
           %decompose
-          a=num.pardecomp(t,y,'mode','struct',...
+          pd_struct=num.pardecomp(t,y,'mode','pd_struct',...
             'polynomial',numel(poly_coeffs),...
             'sinusoidal',sin_periods_assumed...
           );
           %show results
-          num.plot_pardecomp(a);
+          num.plot_pardecomp(pd_struct);
+        % returns the design matrix
         case 'design'
-          %ge the period
+          %get the period(s)
+          if ~isempty(p.Results.pd_struct)
+            %TODO
+          end
           T=p.Results.sinusoidal;
           % init design matrix
           A=zeros(ny,np+2*ns);
@@ -181,6 +249,7 @@ classdef num
           end
           %outputs
           out=A;
+        % returns the y vector
         case 'model'
           %sanity
           assert(~isempty(p.Results.x),[mfilename,': need input argument ''x''.'])
@@ -194,7 +263,8 @@ classdef num
           end
           %outputs
           out=y_mod;
-        case 'struct'
+        % returns struct with x coefficients and some more stuff
+        case 'pd_struct'
           %get the design matrix
           A=num.pardecomp(t,y,varargin{:},'mode','design');
           %solve the system of linear equations
@@ -207,51 +277,6 @@ classdef num
           out=num.save_pardecomp(t,p.Results.sinusoidal,x,y,y_mod,y_res,np,ns);
       otherwise
         error([mfilename,': unknown output mode ''',p.Results.mode,'''.'])
-      end
-    end
-    % shortcut for [s.field(:,column)]
-    function out=struct_deal(s,field,line,column)
-      if ~xor(isempty(line),isempty(column))
-        error([mfilename,': (only) one of ''line'' or ''column'' must be empty.'])
-      end
-      if isempty(line)
-        out=zeros(size(s(1).(field),1),numel(s));
-        for i=1:numel(s)
-          out(:,i)=s(i).(field)(:,column);
-        end
-      else
-        out=zeros(numel(s),size(s(1).(field),2));
-        for i=1:numel(s)
-          out(i,:)=s(i).(field)(line,:);
-        end
-      end
-    end
-    % converts cell array to numeric array if all entries are numeric
-    function inout=cell(inout)
-      if iscell(inout) && all(cellfun(@isnumeric,inout))
-        inout=cell2mat(inout);
-      end
-    end
-    function out=odd(in)
-      if ~isscalar(in)
-        out=arrayfun(@num.odd,in);
-        return
-      end
-      if mod(in,2)==0
-        out=in-sign(in);
-      else
-        out=in;
-      end
-    end
-    function out=even(in)
-      if ~isscalar(in)
-        out=arrayfun(@num.even,in);
-        return
-      end
-      if mod(in,2)==0
-        out=in;
-      else
-        out=in-sign(in);
       end
     end
     function x_opt=param_search1(fun,x,varargin)
@@ -464,6 +489,39 @@ classdef num
         out=transpose(arrayfun(@(i) num.rms(x(i,:),w(i,:),0),1:size(x,1)));
       otherwise
         error(['dim=',num2str(dim),' not implemented.'])
+      end
+    end
+    function out=cov(x,y)
+      n=size(x,2);
+      if n==0
+        out=0;
+        return
+      end
+      if any(size(x)~=size(y))
+        out=NaN;
+        return
+      end
+      out=size(1,n);
+      for i=1:n
+        out(i)=sum(...
+          (x(:,i)-mean(x(:,i))).*...
+          (y(:,i)-mean(y(:,i)))...
+        )/size(x,1);
+      end
+    end
+    function out=corrcoef(x,y)
+      n=min([size(x,2),size(y,2)]);
+      if n==0
+        out=0;
+        return
+      end
+      out=size(1,n);
+      for i=1:n
+        if any([std(x(:,i)),std(y(:,i))]==0)
+          out(i)=0;
+        else
+          out(i)=num.cov(x(:,i),y(:,i))./std(x(:,i),1)./std(y(:,i),1);
+        end
       end
     end
   end
