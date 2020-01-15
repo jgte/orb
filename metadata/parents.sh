29cd1c1fe1251bbb5409936c55338bfcf4c221fe
jgte <5040364+jgte@users.noreply.github.com>
2020-01-15 18:18:32 +0000

    added script to resolve metadata parents

---
+++ metadata/parents.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -ue
+
+METADATADIR=$(cd $(dirname BASH_SOURCE);pwd)
+DEBUG=false
+
+function echoerr(){ echo $@ 1>&2; }
+
+[ $# -eq 0 ] && IN=$(cat) || IN=$@
+
+$DEBUG && echoerr "IN=$IN"
+
+for i in $@ $IN
+do
+  $DEBUG && echoerr "i=$i"
+  [[ "${i/\/}" == "$i" ]] || i="${i%/*}"
+  [[ "${i/.yaml}" == "$i" ]] && i="$i.yaml"
+  if [ ! -e $i ]
+  then
+    echoerr "ERROR: Cannot find metadata $i"
+    exit 3
+  fi
+  for j in submetadata sources
+  do
+    $DEBUG && echoerr "niet $j $i"
+    OUT=$(niet $j $i || echo "ERROR") 
+    $DEBUG && echoerr "OUT=$OUT"
+    [[ "${OUT/ERROR}" == "$OUT" ]] && $BASH_SOURCE $OUT
+    echo $i
+  done
+done | sort -u
