7e1adf03e697d09e6b152409a20ac377dd75480b
Joao De Teixeira da Encarnacao <j.g.deteixeiradaencarnacao@tudelft.nl>
2021-05-07 17:38:18 +0200

    added script to link duplicate files between metadata collections

---
+++ metadata/link-dup-metadatafiles.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -u
+
+DIR=$(cd $(dirname $BASH_SOURCE);pwd)
+SOURCE=$DIR/$1
+SINK=$DIR/$2
+
+if [ ! -d "$SOURCE" ]
+then
+  echo "ERROR: cannot find dir '$SOURCE'"
+  exit 3
+fi
+if [ ! -d "$SINK" ]
+then
+  echo "ERROR: cannot find dir '$SINK'"
+  exit 3
+fi
+
+cd $SINK
+
+for i in $SINK/*.yaml
+do
+  j="$SOURCE/$(basename $i)"
+  if [ -e "$j" ] && diff -q "$i" "$j" > /dev/null
+  then
+    ln -sfv "../$(basename $i)" .
+  fi
+done
+
+cd - > /dev/null
