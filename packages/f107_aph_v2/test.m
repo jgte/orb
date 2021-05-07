b18e1dddcd2eb6bec523c602d5f6c509cb9eb62b
Joao De Teixeira da Encarnacao <5040364+jgte@users.noreply.github.com>
2021-05-07 17:43:56 +0200

    cosmetic changes

--- packages/f107_aph_v2/test.m
+++ packages/f107_aph_v2/test.m
@@ -1,6 +1,6 @@
 function out=test(t)
   if ~exist('t','var') || isempty(t)
-    t=datetime(2003,4,1,0,0,0):days(7):datetime(2017,6,1,0,0,0);
+    t=datetime(2003,4,1,0,0,0):days(7):datetime(2018,1,1,0,0,0);
   end
 
   [out.F107A, out.F107, out.APH]=f107_aph(t);
@@ -12,7 +12,7 @@ function out=test(t)
     'plot_legend_location','none',...
     'plot_line_width',4 ...
   );
-  
+
   %TODO: implement solarflux class from ftp://ftp.gfz-potsdam.de/pub/home/obs/Kp_ap_Ap_SN_F107
-  
+
 end
