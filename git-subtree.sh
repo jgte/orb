ef29590af19d5f120ddd9a6c7215a991d1d4c33c
Joao De Teixeira da Encarnacao <j.g.deteixeiradaencarnacao@tudelft.nl>
2021-05-20 18:04:02 +0200

    udpate git-subree.sh to be more robust and use the --squash option; removed gnss-toolbox

--- git-subtree.sh
+++ git-subtree.sh
@@ -8,9 +8,20 @@ fi
 
 [[ "${@/echo}" == "$@" ]] && ECHO= || ECHO=echo
 
-DIR=$(cd $(dirname $BASH_SOURCE);pwd)
+# https://gist.github.com/SKempin/b7857a6ff6bddb05717cc17a44091202
+case $1 in
+  add|pull) ARGS="--squash" ;;
+  push)     ARGS=           ;;
+  *)
+    echo "ERROR: cannot handle operation '$1'"
+    exit 3
+  ;;
+esac
 
+DIR=$(cd $(dirname $BASH_SOURCE);pwd)
 while IFS= read -r line || [[ -n "$line" ]]; do
+  line=$(echo "$line" | sed 's:#.*::g')
+  [ -z "$line" ] && continue
   # https://www.yeahshecodes.com/git/a-simple-git-subtree-tutorial
-  $ECHO git subtree $1  $line || true
+  $ECHO git subtree $1  $line $ARGS || true
 done < $DIR/${BASH_SOURCE%.sh}.list
