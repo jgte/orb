41bde07c064bf1709e7c5087aedd8aa212b08684
Joao De Teixeira da Encarnacao <j.g.deteixeiradaencarnacao@tudelft.nl>
2021-06-28 16:52:35 +0100

    improved input handling

--- metadata/link-dup-metadatafiles.sh
+++ metadata/link-dup-metadatafiles.sh
@@ -1,19 +1,59 @@
 #!/bin/bash -u
 
 DIR=$(cd $(dirname $BASH_SOURCE);pwd)
-SOURCE=$DIR/$1
-SINK=$DIR/$2
 
-if [ ! -d "$SOURCE" ]
+#inits
+SOURCE=
+SINK=
+ECHO=
+for i in "$@"
+do
+  case "$i" in
+    help)
+      echo "Create links between metadata subdirs. Options are:"
+      grep ') #' $BASH_SOURCE \
+        | grep -v grep \
+        | grep -v sed \
+        | sed 's:) #:|:g' \
+        | column -t -s \|
+      exit
+    ;;
+    echo) #show what would have been done but don't do anything
+      ECHO=echo
+    ;;
+    source=*) #mandatory: define the directory to which the links will point to
+      SOURCE=${i/source=}
+      if [ ! -d "$SOURCE" ]
+      then
+        echo "ERROR: cannot find dir '$SOURCE'"
+        exit 3
+      fi
+    ;;
+    sink=*) #mandatory: define the directory inside which the links will reside
+      SINK=${i/sink=}
+      if [ ! -d "$SINK" ]
+      then
+        echo "ERROR: cannot find dir '$SINK'"
+        exit 3
+      fi
+    ;;
+    *)
+      echo "ERROR: cannot handle input '$i'"
+    ;;
+  esac
+done
+
+if [ -z "$SOURCE" ]
 then
-  echo "ERROR: cannot find dir '$SOURCE'"
+  echo "ERROR: need input 'source=...; call '$BASH_SOURCE help' for more info"
   exit 3
 fi
-if [ ! -d "$SINK" ]
+if [ -z "$SINK" ]
 then
-  echo "ERROR: cannot find dir '$SINK'"
+  echo "ERROR: need input 'sink=...; call '$BASH_SOURCE help' for more info"
   exit 3
 fi
+
 
 cd $SINK
 
@@ -22,7 +62,7 @@ do
   j="$SOURCE/$(basename $i)"
   if [ -e "$j" ] && diff -q "$i" "$j" > /dev/null
   then
-    ln -sfv "../$(basename $SOURCE)/$(basename $i)" .
+    $ECHO ln -sfv "../$(basename $SOURCE)/$(basename $i)" .
   fi
 done
 
