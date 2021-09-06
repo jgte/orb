6c937fc87ac003133f92f7ce270d46897fa899b2
Joao De Teixeira da Encarnacao <j.g.deteixeiradaencarnacao@tudelft.nl>
2021-09-06 20:09:38 +0200

    fixed issue with files with blanks

--- grep-m.sh
+++ grep-m.sh
@@ -2,11 +2,11 @@
 
 DIRNOW=$( cd $(dirname $BASH_SOURCE); pwd )
 project=$(
-  [ -e $DIRNOW/project.yaml ] \
-  && grep 'name:' $DIRNOW/project.yaml \
-  || grep 'name:' $DIRNOW/default.yaml \
+  [ -e "$DIRNOW/project.yaml" ] \
+  && grep 'name:' "$DIRNOW/project.yaml" \
+  || grep 'name:' "$DIRNOW/default.yaml" \
 )
 project=$(echo $project| awk -F\: '{print $2}')
 project=${project// }
 
-grep --color=always "$@" $DIRNOW/*.m $DIRNOW/metadata/$project/*.yaml
+grep --color=always "$@" "$DIRNOW/"*.m "$DIRNOW/metadata/$project/"*.yaml
